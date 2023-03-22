17911bba47c6a490a604937219620ffbcb78d0c2
João Lucas <lucasbanana96@gmail.com>
2023-03-22 15:40:33 -0300

    Update ex-02.sql

--- Unidade 1/ex-02.sql
+++ Unidade 1/ex-02.sql
@@ -1,4 +1,10 @@
 1 - Qual o nome do projeto que tem mais atividades realizadas? 
+select p.descricao, count(a.projeto_id)
+from projetos p 
+join atividadesprojetos a on a.projeto_id = p.id
+group by p.descricao
+order by count(a.projeto_id) DESC 
+limit 1;
 
 2 - Exibir nome de cada projeto e de seu funcionário responsável. Exibir o nome do projeto mesmo que não haja funcionário responsável.
 
@@ -16,6 +22,6 @@ left join projetos p on p.funcionario_responsavel_id = f.id;
 4 - Aumente em 20% o salário dos funcionários que são responsáveis por projetos. 
 
 5 - Aloque para o projeto Gama a atividade Implantacao do produto. 
+insert into atividadesprojetos values (3,7);
 
-6 - Reduza em 10% o salário dos funcionários dos departamentos cuja média salarial seja
-maior que a média salarial da empresa.
+6 - Reduza em 10% o salário dos funcionários dos departamentos cuja média salarial seja maior que a média salarial da empresa.
